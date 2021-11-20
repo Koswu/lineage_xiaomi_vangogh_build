ff070e5be3347ae415d474a648c6c63fae0daca7
koswu <ws00298046@163.com>
2021-11-20 15:51:19 +0800

    update

--- sync.sh
+++ sync.sh
@@ -5,9 +5,7 @@ cd /tmp/rom
 
 
 # Repo init command, that -device,-mips,-darwin,-notdefault part will save you more time and storage to sync, add more according to your rom and choice. Optimization is welcomed! Let's make it quit, and with depth=1 so that no unnecessary things.
-repo init --depth=1 git://github.com/LineageOS/android.git -b lineage-18.1 
-#-g default,-device,-mips,-darwin,-notdefault
-ls -al
+repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1 -g default,-device,-mips,-darwin,-notdefault
 
 #rm -rf .repo/local_manifests
 
